8c7009036681d394a89101881c769893de658117
Amogh Maroju <amogh.gyana@gmail.com>
2024-06-25 16:13:06 -0500

    finished part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 const App = () => {
+  // create notes for all the cards
+  const createNotes = (noteItem) => {
+    return (
+      <Note
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+      />
+    );
+  };
+
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
